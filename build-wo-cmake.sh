fd4f10376a63163119126f0d40ab8599d6358dbf
Folkert van Heusden <folkert@vanheusden.com>
2021-12-15 14:16:02 +0100

    Debug symbols in lock_tracer library

--- build-wo-cmake.sh
+++ build-wo-cmake.sh
@@ -1,1 +1,1 @@
-g++ -fPIC -std=c++1y lock_tracer.cpp -shared -o lock_tracer.so -Wl,--whole-archive -ljansson -Wl,--no-whole-archive
+g++ -ggdb3 -fPIC -std=c++1y lock_tracer.cpp -shared -o lock_tracer.so -Wl,--whole-archive -ljansson -Wl,--no-whole-archive
