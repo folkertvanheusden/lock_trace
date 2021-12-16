e16a92d329a575a4ea38c123278c142f5bbd7771
Folkert van Heusden <folkert@vanheusden.com>
2021-12-16 11:53:30 +0100

    branch for file-backed data

--- lock_tracer.cpp
+++ lock_tracer.cpp
@@ -9,6 +9,7 @@
 #include <dlfcn.h>
 #include <errno.h>
 #include <execinfo.h>
+#include <fcntl.h>
 #include <jansson.h>
 #include <limits.h>
 #include <map>
@@ -24,10 +25,12 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/resource.h>
+#include <sys/stat.h>
 #include <sys/syscall.h>
 #include <sys/sysinfo.h>
 #include <sys/time.h>
 #include <sys/types.h>
+
 
 #if JSON_INTEGER_IS_LONG_LONG
 #else
@@ -45,6 +48,8 @@
 
 uint64_t n_records = 16777216, emit_count_threshold = n_records / 10;
 size_t length = 0;
+int fd = -1;
+
 bool verbose = false;
 
 bool fork_warning = false;
@@ -752,8 +757,22 @@ void __attribute__ ((constructor)) start_lock_tracing()
 
 	fprintf(stderr, "Tracing max. %zu records\n", n_records);
 
+    fd = open("measurements.dat", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+		fprintf(stderr, "ERROR: cannot create data file: %s\n", strerror(errno));
+		color("\033[0m");
+		_exit(1);
+    }
+
 	length = n_records * sizeof(lock_trace_item_t);
-	items = (lock_trace_item_t *)mmap(nullptr, length, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    if (ftruncate(fd, length) == -1) {
+		fprintf(stderr, "ERROR: problem reserving space on disk: %s\n", strerror(errno));
+		color("\033[0m");
+		_exit(1);
+    }
+
+	items = (lock_trace_item_t *)mmap(nullptr, length, PROT_WRITE | PROT_READ, MAP_SHARED | MAP_POPULATE, fd, 0);
 
 	if (items == MAP_FAILED) {
 		fprintf(stderr, "ERROR: cannot allocate %lu bytes of memory (reduce with the \"TRACE_N_RECORDS\" environment variable): %s\n", length, strerror(errno));
@@ -803,7 +822,16 @@ void exit(int status)
 	uint64_t end_ts = get_ns();
 
 	color("\033[0;31m");
-	fprintf(stderr, "Lock tracer terminating... (path: %s)\n", get_current_dir_name());
+
+    unsigned long count = items_idx;
+	fprintf(stderr, "Lock tracer terminating with %lu records (path: %s, %zu bytes)\n", count, get_current_dir_name(), length);
+
+    if (msync(items, length, MS_SYNC) == -1)
+        fprintf(stderr, "Problem pushing data to disk: %s\n", strerror(errno));
+
+    munmap(items, length);  // TODO error checking
+
+    close(fd);
 
 	if (!items) {
 		fprintf(stderr, "No items recorded yet\n");
@@ -871,6 +899,8 @@ void exit(int status)
 
 		emit_key_value(fh, "exe_name", exe_name);
 
+		emit_key_value(fh, "measurements", "measurements.dat");
+
 		emit_key_value(fh, "cnt_mutex_trylock", cnt_mutex_trylock);
 		emit_key_value(fh, "cnt_rwlock_try_rdlock", cnt_rwlock_try_rdlock);
 		emit_key_value(fh, "cnt_rwlock_try_timedrdlock", cnt_rwlock_try_timedrdlock);
@@ -894,82 +924,7 @@ void exit(int status)
 		fprintf(fh, "%s\n", json_dumps(m_obj, JSON_COMPACT));
 		json_decref(m_obj);
 
-		for(uint64_t i = 0; i<n_rec_inserted; i++) {
-			caller_str[0] = 0x00;
-
-#ifndef WITH_TIMESTAMP
-			items[i].timestamp = 0;
-#endif
-
-#ifdef WITH_BACKTRACE
-			for(int j=0; j<CALLER_DEPTH; j++)
-				sprintf(&caller_str[strlen(caller_str)], "%p,", items[i].caller[j]);
-#endif
-
-			const char *action_name = "?";
-			bool rw_lock = false;
-
-			if (items[i].la == a_lock)
-				action_name = "lock";
-			else if (items[i].la == a_unlock)
-				action_name = "unlock";
-			else if (items[i].la == a_thread_clean)
-				action_name = "tclean";
-			else if (items[i].la == a_error)
-				action_name = "error";
-			else if (items[i].la == a_r_lock)
-				action_name = "readlock", rw_lock = true;
-			else if (items[i].la == a_w_lock)
-				action_name = "writelock", rw_lock = true;
-			else if (items[i].la == a_rw_unlock)
-				action_name = "rwunlock", rw_lock = true;
-			else if (items[i].la == a_init)
-				action_name = "init";
-			else if (items[i].la == a_destroy)
-				action_name = "destroy";
-			else if (items[i].la == a_rw_init)
-				action_name = "rw_init";
-			else if (items[i].la == a_rw_destroy)
-				action_name = "rw_destroy";
-
-			json_t *obj = json_object();
-
-			if (items[i].thread_name[0])
-				json_object_set(obj, "thread_name", json_string(items[i].thread_name));
-			else
-				json_object_set(obj, "thread_name", json_string("?"));
-
-			json_object_set(obj, "type", json_string("data"));
-			json_object_set(obj, "t", json_integer(i));
-			json_object_set(obj, "lock", json_integer((long long unsigned int)items[i].lock));
-			json_object_set(obj, "tid", json_integer(items[i].tid));
-			json_object_set(obj, "action", json_string(action_name));
-			json_object_set(obj, "caller", json_string(caller_str));
-			json_object_set(obj, "timestamp", json_integer(items[i].timestamp));
-			json_object_set(obj, "lock_took", json_integer(items[i].lock_took));
-			json_object_set(obj, "rc", json_integer(items[i].rc));
-
-			if (rw_lock) {
-				json_object_set(obj, "rwlock_readers", json_integer(items[i].rwlock_innards.__readers));
-				json_object_set(obj, "rwlock_writers", json_integer(items[i].rwlock_innards.__writers));
-				json_object_set(obj, "cur_writer",     json_integer(items[i].rwlock_innards.__cur_writer));
-			}
-			else {
-				json_object_set(obj, "mutex_count", json_integer(items[i].mutex_innards.__count));
-				json_object_set(obj, "mutex_owner", json_integer(items[i].mutex_innards.__owner));
-				json_object_set(obj, "mutex_kind",  json_integer(items[i].mutex_innards.__kind));
-#ifdef __x86_64__
-				json_object_set(obj, "mutex_spins",  json_integer(items[i].mutex_innards.__spins));
-				json_object_set(obj, "mutex_elision",  json_integer(items[i].mutex_innards.__elision));
-#endif
-			}
-
-			fprintf(fh, "%s\n", json_dumps(obj, JSON_COMPACT));
-			json_decref(obj);
-		}
-
 		if (fh != stderr) {
-			fsync(fileno(fh));
 			fclose(fh);
 
 			sync();
@@ -982,8 +937,6 @@ void exit(int status)
 	items_idx = n_records;
 
 	delete tid_names;
-
-    munmap(items, length);
 
     // dump core
 	color("\033[0;31m");
