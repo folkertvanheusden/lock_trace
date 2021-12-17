a3ee03e1be1a1f234b428b128e2ca59980bf021e
folkert van heusden <mail@vanheusden.com>
2021-12-17 16:27:42 +0100

    do not erase end()

--- analyzer.cpp
+++ analyzer.cpp
@@ -200,8 +200,9 @@ auto do_find_double_un_locks_mutex(const lock_trace_item_t *const data, const si
 
 					put_lock_error(&out, mutex, lae_not_owner, hash, i);
 				}
-
-				it->second.erase(tid_it);
+				else {
+					it->second.erase(tid_it);
+				}
 
 				if (it->second.empty())
 					locked.erase(it);
@@ -465,8 +466,9 @@ auto do_find_double_un_locks_rwlock(const lock_trace_item_t *const data, const s
 
 						put_lock_error(&out, rwlock, lae_not_owner, hash, i);
 					}
-
-					it->second.erase(tid_it);
+					else {
+						it->second.erase(tid_it);
+					}
 
 					if (it->second.empty())
 						r_locked.erase(it);
